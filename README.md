917b8b0fb04598329fc2fe745b622bd045f89bf3
tksml13 <tksml13@kpcard.co.kr>
2024-07-23 15:30:17 +0900

    README, postman 추가

--- README.md
+++ README.md
@@ -4,9 +4,26 @@ ELK 검색엔진
 ### 1. docker-compose 실행
    docker-compose up --build -d
 
-### 2. docker 재실행
-   docker-compose down
-   docker-compose up --build -d
+### 2. nori 플러그인 설치 & 인덱스 등록
+   init-elasticsearch.sh
+
+### 3. 엘라스틱 서치 컨테이너 재시작
+
+### 4. kibana index management
+   http://localhost:5601/
+   create index pattern
+
+### 5. 데이터베이스
+   테이블 생성
+   데이터 insert
+
+### Process
+   - 위 환경 설정이 완료 된 후, 검색엔진을 통해 사용 가능
+   - postman_collection.json을 통해 테스트 가능
+   - nori를 기반으로 형태소 분석
+   - logstatsh를 통해 업데이트 시간을 기준으로 데이터 동기화
 
 ## referrence
-https://velog.io/@yoonene/Docker%EB%A1%9C-%EC%84%9C%EB%B2%84%EC%97%90%EC%84%9C-ElasticsearchKibananori-plugin-%EA%B5%AC%EB%8F%99%ED%95%98%EA%B8%B0
+   https://velog.io/@yoonene/Docker%EB%A1%9C-%EC%84%9C%EB%B2%84%EC%97%90%EC%84%9C-ElasticsearchKibananori-plugin-%EA%B5%AC%EB%8F%99%ED%95%98%EA%B8%B0
+   http://kimjmin.net/2019/08/2019-08-how-to-analyze-korean/
+   
--- elasticsearch/init-elasticsearch.sh
+++ elasticsearch/init-elasticsearch.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Wait for Elasticsearch to start up
+# Elasticsearch 완전히 켜졌는지 확인
 until curl -s http://localhost:9200/_cluster/health?wait_for_status=yellow&timeout=50s; do
   echo 'Waiting for Elasticsearch...' >> /usr/share/elasticsearch/config/init-elasticsearch.log
   sleep 10
@@ -10,15 +10,15 @@ done
 echo "Installing nori plugin..." >> /usr/share/elasticsearch/config/init-elasticsearch.log
 elasticsearch-plugin install analysis-nori
 
-# 10초 대기
+# 5초 대기
 echo "Waiting for 10 seconds..." >> /usr/share/elasticsearch/config/init-elasticsearch.log
 sleep 5
 
 echo "Elasticsearch started, applying template..." >> /usr/share/elasticsearch/config/init-elasticsearch.log
 
-# Apply the index template
+# Apply the index
 curl -X PUT localhost:9200/ocr_product_keyword?pretty=true -d @/usr/share/elasticsearch/config/ocr_product_keyword.json -H "Content-Type:application/json"
-echo "Template applied successfully" >> /usr/share/elasticsearch/config/init-elasticsearch.log
+echo "index applied successfully" >> /usr/share/elasticsearch/config/init-elasticsearch.log
 
-# Keep the script running to avoid container exit
+# 스크립트 끝난 뒤 컨테이너 종료 방지
 tail -f /dev/null
